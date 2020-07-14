2ff4b22d7537e66f52d8bd086223f079c18f756d
Corentin <job.corentin@outlook.fr>
2020-07-14 10:56:13 +0200

    Menu for build added

---
+++ Hell/Assets/COJ/COJ_Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey("b"))
+        {
+            LoadScene(0);
+        }
+    }
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
