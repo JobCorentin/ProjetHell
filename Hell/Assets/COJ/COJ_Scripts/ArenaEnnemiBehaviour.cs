5d9b9580dacfec7a15472acec78197109b4ba735
Corentin <job.corentin@outlook.fr>
2020-07-16 21:48:31 +0200

    Arena Trigger manager added

---
+++ Hell/Assets/COJ/COJ_Scripts/ArenaEnnemiBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinemachine
+{
+    public class ArenaEnnemiBehaviour : MonoBehaviour
+    {
+        public ArenaScript arena;
+        public EnnemiController enemiStat;
+
+        private void Start()
+        {
+            enemiStat = gameObject.GetComponent<EnnemiController>();
+        }
+
+        void Update()
+        {
+            if(enemiStat.health <= 0)
+            {
+                arena.arenaEnemi.Remove(gameObject);
+            }
+        }
+    }
+}
+
---
+++ Hell/Assets/COJ/COJ_Scripts/ArenaScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cinemachine
+{
+    public class ArenaScript : MonoBehaviour
+    {
+        public CinemachineConfiner confiner;
+        public GameObject arena;
+        public GameObject confinerCollider;
+        public List<GameObject> arenaEnemi;
+
+        bool hasActivated = false;
+
+        private void Start()
+        {
+            arena.SetActive(false);
+            confiner = GameObject.Find("CM vcam1").GetComponent<CinemachineConfiner>();
+        }
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "Player" && hasActivated == false)
+            {
+                arena.SetActive(true);
+                confiner.m_BoundingShape2D = confinerCollider.GetComponent<PolygonCollider2D>();
+                hasActivated = true;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if(arenaEnemi.Count <= 0)
+            {
+                confiner.m_BoundingShape2D = null;
+                arena.SetActive(false);
+            }
+        }
+    }
+}
+
