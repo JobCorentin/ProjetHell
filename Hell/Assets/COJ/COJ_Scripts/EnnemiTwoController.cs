6e83113986ea43970c1706c4a75a1379cfecfad1
Corentin <job.corentin@outlook.fr>
2020-07-16 11:06:14 +0200

    Ennemi two WIP

---
+++ Hell/Assets/COJ/COJ_Scripts/EnnemiTwoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiTwoController : EnnemiController
+{
+    public EnnemiDetection ennemiDetection;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        
+    }
+}
