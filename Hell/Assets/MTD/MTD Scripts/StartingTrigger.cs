70b8c2b99bb623a1923fe764b373ac03ca8f6ed4
Matis Duperray <m.duperray@rubika-edu.com>
2020-08-01 16:14:30 +0200

    Add des sons

---
+++ Hell/Assets/MTD/MTD Scripts/StartingTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingTrigger : MonoBehaviour
+{
+    [TextArea]
+    public string Instruction = ("Cet objet sert à lancer une musique quand le jeu se lance.");
+
+    [Space(20)]
+    [Header("Music")]
+    public AudioClip musicClip = null;
+    [Range(0f, 5f)] public float musicVolumeModificator = 1f;
+
+    private void Start()
+    {
+        SoundManager.instance.PlayMusic(musicClip, musicVolumeModificator);
+        Destroy(gameObject);
+    }
+}
