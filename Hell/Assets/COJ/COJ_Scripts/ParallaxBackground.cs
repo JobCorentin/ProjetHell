d92ddca62b79ab3779ed4bc0d2fae487d909924c
Corentin <job.corentin@outlook.fr>
2020-08-11 14:11:27 +0200

    Parallax Added

---
+++ Hell/Assets/COJ/COJ_Scripts/ParallaxBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+
+    public Transform cameraTransform;
+    Vector3 lastCameraPosition;
+    [SerializeField]public Vector2 parallaxEffectMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+    }
+}
