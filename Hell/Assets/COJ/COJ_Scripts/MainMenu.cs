0c4dc135c3a2dd0d19926d60a06563c1eb370e3a
Corentin <job.corentin@outlook.fr>
2020-08-18 18:39:38 +0200

    Menu Principal WIP

---
+++ Hell/Assets/COJ/COJ_Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
