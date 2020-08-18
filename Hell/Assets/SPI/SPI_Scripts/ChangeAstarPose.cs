1fd323cda9f73a0d18cde17c5b3c67daecb9ec98
Cardo <S.picardat@rubika-edu.com>
2020-08-18 14:35:58 +0200

    Test Astar

---
+++ Hell/Assets/SPI/SPI_Scripts/ChangeAstarPose.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ChangeAstarPose : MonoBehaviour
+{
+    public AstarData data;
+    GridGraph gg;
+
+    public GameObject newGridPose;
+
+
+    private void Start()
+    {
+
+        gg = data.gridGraph;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Palyer")
+        {
+            //pathfinder.GetComponent<AstarPath>.center = newGridPose.transform.position;
+            gg.center = newGridPose.transform.position;
+
+            //astar.Scan();
+        }
+    }
+
+}
