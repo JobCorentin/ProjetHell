149955393c3c11f761fac52ea937d9a2d56819ad
Cardo <S.picardat@rubika-edu.com>
2020-08-19 16:43:20 +0200

    fix boom

--- Hell/Assets/AUM/AUM_Scripts/EnnemiController.cs
+++ Hell/Assets/AUM/AUM_Scripts/EnnemiController.cs
@@ -68,6 +68,9 @@ public class EnnemiController : MonoBehaviour
     [HideInInspector] public Vector2 initialPosition;
     [HideInInspector] public int initialHealth;
 
+
+    [HideInInspector] public bool hasNoHP;
+
     // Start is called before the first frame update
     public virtual void Start()
     {
@@ -195,7 +198,7 @@ public class EnnemiController : MonoBehaviour
 
     public IEnumerator TakeDamage(int amount)
     {
-        if (hasSpawn == true)
+        if (hasSpawn == true && hasNoHP == false)
         {
 
             MovementController.mC.canDoubleJump = true;
@@ -225,9 +228,11 @@ public class EnnemiController : MonoBehaviour
                 yield return new WaitForSeconds(0.05f);
 
                 sr.material = defautlMaterial;
+                hasNoHP = false;
             }
-            else if (health <= 0)
+            else if (health <= 0 )
             {
+                hasNoHP = true;
                 Die();
             }
 
