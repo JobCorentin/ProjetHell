af46ab5110a313374e12b33d364ffb53fbd77869
Cardo <S.picardat@rubika-edu.com>
2020-08-25 12:16:40 +0200

    Doorprefab

---
+++ Hell/Assets/SPI/SPI_Scripts/BossDoor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDoor : MonoBehaviour
+{
+    public List<SatueBossGuard> statues;
+    public GameObject TpBossPose;
+
+    [HideInInspector] public int statuesToDestroy;
+
+    public bool openDoor;
+    private void Update()
+    {
+        if(openDoor == false)
+        {
+            foreach (SatueBossGuard stat in statues)
+            {
+                if (stat.destroyed == true)
+                {
+                    statuesToDestroy++;
+                }
+            }
+            if (statuesToDestroy == statues.Count)
+            {
+                openDoor = true;
+            }
+            else
+            {
+                openDoor = false;
+                statuesToDestroy = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (openDoor == true)
+            {
+                collision.transform.position = TpBossPose.transform.position;
+            }
+        }
+    }
+}
---
+++ Hell/Assets/SPI/SPI_Scripts/SatueBossGuard.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatueBossGuard : MonoBehaviour
+{
+    public bool destroyed;
+}
