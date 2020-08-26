b698ccea497e5f361d522c30ea80ec4e55f57d33
Aurélien <orelien.mary@gmail.com>
2020-08-26 11:03:11 +0200

    Tuto Template

---
+++ Hell/Assets/AUM/AUM_Scripts/TutoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoController : MonoBehaviour
+{
+    public Animator tutoAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        tutoAnimator.SetBool("Appear", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            tutoAnimator.SetBool("Appear", false);
+    }
+}
