a4f0daa463def06ce7642491ca6bbdaea0f0b766
Corentin <job.corentin@outlook.fr>
2020-08-30 12:16:42 +0200

    Ember prefab in LD + Urn integrated + Lamp integrated

--- Hell/Assets/SPI/SPI_Scripts/Healthpack.cs
+++ Hell/Assets/SPI/SPI_Scripts/Healthpack.cs
@@ -7,12 +7,19 @@ public class Healthpack : PropsBehaviour
     public int heal;
     bool canHeal =true;
 
+    public GameObject fxHolder;
+
     public void HealPlayer()
     {
         if (canHeal && HealthManager.hm.life < HealthManager.hm.initialLife)
         {
                 canHeal = false;
                 HealthManager.hm.life += heal;
+        }
+
+        if(health <= 0)
+        {
+            fxHolder.SetActive(false);
         }
     }
 }
